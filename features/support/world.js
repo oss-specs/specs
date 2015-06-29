927908e296601315208e6828d7da56df719e3263
Jim Cresswell <jim.cresswell@gmail.com>
2015-06-29 13:10:08 +0100

    Docs for world.js

--- features/support/world.js
+++ features/support/world.js
@@ -1,1 +1,8 @@
+/**
+ * This file will be automatically required by CucumberJS.
+ * Currently not overriding the default World object.
+ */
+
+// Require the 'should' assertion libary which modifies
+// the Object prototype to include assertions.
 require('should');
