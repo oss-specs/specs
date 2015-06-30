fda6a6af75ebbe39646b6e826a1fb7646e98259a
jimCresswell <jim.cresswell@gmail.com>
2015-06-30 15:21:20 +0100

    Ignore test files in the public folder. Fix linting in index.js.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+"use strict";
+/* eslint new-cap: 0 */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'a lovely specification app.' });
 });
 
