b6c77d24624eb78b881388aed8a73509fc2f6179
jimCresswell <jim.cresswell@gmail.com>
2015-07-09 17:26:48 +0100

    Given a Git https URL checkout a copy of the repo into the public folder.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,29 @@
 var express = require('express');
 var router = express.Router();
 
+var getProject = require("../lib/specifications/getProject");
+
 router.get('/', function(req, res) {
-  res.render('index');
+  var repoUrl = req.query.repo_url;
+
+  // If there is no URL query param then
+  // immediately render the page.
+  if (!repoUrl) {
+    res.render('index');
+    return;
+  }
+
+  getProject(repoUrl)
+    .then(function(data) {
+
+      // Redirect to the features page.
+      res.redirect('/features');
+    })
+    .catch(function(err) {
+      res
+        .status(500)
+        .send(err);
+    });
 });
 
 module.exports = router;
