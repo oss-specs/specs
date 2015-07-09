d0cc5909bc767d7d5cd69c60f5bfb7804739145c
jimCresswell <jim.cresswell@gmail.com>
2015-07-09 09:12:38 +0100

    Link to the features page from the front page.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('Front page.');
+  res.send('<h1>Front page</h1><h2><a href="/features">Features</a></h2>');
 });
 
 module.exports = router;
