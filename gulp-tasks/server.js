97317b7d6086f472522ecd829622eb3974e6c8aa
jimCresswell <jim.cresswell@gmail.com>
2015-07-24 11:24:04 +0100

    Add support for development server restart on file change.

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -2,12 +2,26 @@
 // which lists what tasks are available.
 var gulp = require('gulp-help')(require('gulp'));
 
-var server = require( 'gulp-develop-server' );
+// https://github.com/narirou/gulp-develop-server
+// Relies on default or defineable signals from the child
+// server process to detect start success and error
+// http://stackoverflow.com/a/10394457
+var server = require('gulp-develop-server');
 
 var packagejson = require('../package.json');
+var binaryPath = packagejson.bin['module-name'];
+var watchGlobs = [
+  binaryPath,
+  'app.js',
+  'lib/**/*.js',
+  'public/css/**/*.css',
+  'public/javascript/**/*.js',
+  'routes/**/*.js',
+  'views/**/*'
+];
 
 gulp.task('server:start', 'Start serving the app', function(done) {
-  var binaryPath = packagejson.bin['module-name'];
+
   server.listen({
     path: binaryPath,
     env: {
@@ -19,4 +33,9 @@ gulp.task('server:start', 'Start serving the app', function(done) {
 
 gulp.task('server:stop', 'Stop serving the app', function(done) {
   server.kill(undefined, done);
+});
+
+// Note: will exit process on error.
+gulp.task('server:restart', 'Restart the server when defined files change', function() {
+    gulp.watch(watchGlobs, server.restart);
 });
