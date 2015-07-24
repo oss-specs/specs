816d4897f29e4dec8d45f577d15a539f42961e2a
jimCresswell <jim.cresswell@gmail.com>
2015-07-24 09:48:48 +0100

    Create a Gulp server:start task which listens for default start and error messages from the server using process.send, uses gulp-develop-server.

---
+++ gulp-tasks/serve.js
@@ -0,0 +1,18 @@
+// Use the gulp-help plugin which defines a default gulp task
+// which lists what tasks are available.
+var gulp = require('gulp-help')(require('gulp'));
+
+var server = require( 'gulp-develop-server' );
+
+var packagejson = require('../package.json');
+
+gulp.task('server:start', 'Start serving the app', function(done) {
+  var binaryPath = packagejson.bin['module-name'];
+  server.listen({
+    path: binaryPath,
+    env: {
+      NODE_ENV: 'development',
+      PORT: 1337
+    }
+  }, done);
+});
