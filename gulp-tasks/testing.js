425f761534fb8443c512f0b667ffe666e3d15d29
jimCresswell <jim.cresswell@gmail.com>
2015-07-25 09:11:54 +0100

    Update gulp-cucumber to pull in the fixes, update Cucumber standalone to match version used by gulp-cucumber.

--- gulp-tasks/testing.js
+++ gulp-tasks/testing.js
@@ -7,7 +7,6 @@ var runSequence = require('run-sequence').use(gulp);
 
 var cucumber = require('gulp-cucumber');
 
-// Won't work until https://github.com/vgamula/gulp-cucumber/pull/16 is accepted and a new version published.
 // Run all the Cucumber features, doesn't start server
 // Hidden from gulp-help.
 gulp.task('cucumber', false, function() {
@@ -16,7 +15,6 @@ gulp.task('cucumber', false, function() {
   }));
 });
 
-// Also depends on https://github.com/vgamula/gulp-cucumber/pull/16
 gulp.task('test:features', 'Test the features', function(done) {
   runSequence('set-envs',
               'server:start',
