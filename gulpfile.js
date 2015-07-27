41cdf00347810868636ddf06c6041fc2886a7d76
jimCresswell <jim.cresswell@gmail.com>
2015-07-27 11:32:58 +0100

    Add a note to do coverage later.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,6 @@
 // Pull in the tasks from the gulp-tasks directory.
 require('require-dir')('./gulp-tasks');
 
-// "pretest-with-coverage": "npm run pretest",
+// TODO: coverage https://github.com/oss-specs/specs/issues/65.
 // "test-with-coverage": "PORT=1337 node_modules/.bin/istanbul cover node_modules/.bin/cucumber-js --report teamcity --report lcov -- --require features-support -f progress",
-// "posttest-with-coverage": "npm run posttest",
-
 // "upload-coverage-data": "node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info",
-
-// "lint": "node_modules/.bin/eslint ./"
