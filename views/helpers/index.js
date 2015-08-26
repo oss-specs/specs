24e8b683b1e667dec5ba92fecf2e2594b59d401a
jimCresswell <jim.cresswell@gmail.com>
2015-08-26 17:54:39 +0100

    Standardise string to HTML conversion and remove script attack vector in gherkin comments :)

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -3,17 +3,23 @@
 var handlebars = require('hbs').handlebars;
 var htmlEncode = require('ent/encode');
 
-module.exports = {
-  newlinesToBreaks: function(context, options) {
-    return new handlebars.SafeString(context.replace(/\n/g, '<br>'));
-  },
-  docstringToHtml: function(context, options) {
+function getStringConverter(aggregator) {
+  return function(context, options) {
     var content = context.split('\n');
     content = content.reduce(function(previous, current) {
+
+      // Gaurantee rendered content.
       var safeContent = htmlEncode(current) || '&nbsp;';
-      return previous += '<p>' + safeContent + '</p>';
+      return previous += aggregator(safeContent);
     }, '');
+
+    // Render leading whitespace characters.
     content = content.replace(/\s/g, '&nbsp;');
     return new handlebars.SafeString(content);
   }
+}
+
+module.exports = {
+  newlinesToBreaks: getStringConverter(function(safeContent) { return safeContent + '<br>'; }),
+  docstringToHtml: getStringConverter(function(safeContent) { return '<p>' + safeContent + '</p>'; })
 };
