060b1ed1860f15b427c7583eba6aa0a30f76e473
jimCresswell <jim.cresswell@gmail.com>
2015-09-14 14:35:35 +0100

    Allow branch changing based on query parameter. Uses remote branches, breaks the update button once used.

--- routes/project.js
+++ routes/project.js
@@ -5,16 +5,24 @@ var express = require('express');
 var router = express.Router();
 
 var Q = require('q');
+
 var getProjectMetaDataByName = require('../lib/specifications/projectMetaData').getByName;
 var updateProject = require('../lib/specifications/getProject').update;
+var changeBranch = require('../lib/specifications/getProject').changeBranch;
 
 // List of available features in a project.
 router.get('/:projectName', function(req, res, next) {
 
   var projectName = req.params.projectName;
 
+  // Query param casuing a Git update (pull).
   var projectShouldUpdate = (req.query.update === 'true' || !!parseInt(req.query.update));
 
+  // Query param indicating that it should be attempted
+  // to check out the specified branch.
+  var targetBranchName = req.query.branch || false;
+
+  // Render the project page and send to client.
   function render(projectData) {
     var data = {};
     if (projectData) {
@@ -27,6 +35,7 @@ router.get('/:projectName', function(req, res, next) {
     next(err)
   }
 
+  // If the update flag is set then branch change requests will be ingored.
   if (projectShouldUpdate) {
     updateProject(projectName)
       .then(function() {
@@ -34,6 +43,17 @@ router.get('/:projectName', function(req, res, next) {
       })
       .then(render)
       .catch(passError);
+
+  // Change the branch.
+  } else if (targetBranchName) {
+    changeBranch(projectName, targetBranchName)
+      .then(function() {
+        return getProjectMetaDataByName(projectName);
+      })
+      .then(render)
+      .catch(passError);
+
+  // Else, generate the metadata and render the page.
   } else {
     getProjectMetaDataByName(projectName)
       .then(render)
