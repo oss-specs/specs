d00dc7f443d4f4ec3f8c1cc96cf4aa0894fe5d09
jimCresswell <jim.cresswell@gmail.com>
2015-10-19 14:59:06 +0100

    Switch from toggling visibility to switching on stored state in preparation for allow individual scenarios to toggle.

--- public/javascript/feature.js
+++ public/javascript/feature.js
@@ -2,15 +2,29 @@
 
 // Expand/collapse details.
 $(function() {
+  var doExpand = 1
+
   function expandCollapseDetails() {
     var featureDetailsEls = window.document.getElementsByClassName('feature-details');
     var scenarioDetailsEls = window.document.getElementsByClassName('scenario-details');
+
     [].forEach.call(featureDetailsEls, function(el) {
-      el.classList.toggle('collapse');
+      if (doExpand) {
+        el.classList.remove('collapse');
+      } else {
+        el.classList.add('collapse');
+      }
     });
     [].forEach.call(scenarioDetailsEls, function(el) {
-      el.classList.toggle('collapse');
+      if (doExpand) {
+        el.classList.remove('collapse');
+      } else {
+        el.classList.add('collapse');
+      }
     });
+
+    // Toggle expansion on alternative executions.
+    doExpand = doExpand ^ 1;
   }
 
   var expandCollapseDetailsEl = window.document.getElementById('expand-collapse-details');
