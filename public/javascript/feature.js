d8521c8a9ef47830eb017aada981c6df86829af4
jimCresswell <jim.cresswell@gmail.com>
2015-10-19 13:36:13 +0100

    Collapse details on feature pages by default and add collapse-exapnd button for them.

--- public/javascript/feature.js
+++ public/javascript/feature.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// Expand/collapse details.
+$(function() {
+  function expandCollapseDetails() {
+    var featureDetailsEls = window.document.getElementsByClassName('feature-details');
+    var scenarioDetailsEls = window.document.getElementsByClassName('scenario-details');
+    [].forEach.call(featureDetailsEls, function(el) {
+      el.classList.toggle('collapse');
+    });
+    [].forEach.call(scenarioDetailsEls, function(el) {
+      el.classList.toggle('collapse');
+    });
+  }
+
+  var expandCollapseDetailsEl = window.document.getElementById('expand-collapse-details');
+  expandCollapseDetailsEl.addEventListener('click', expandCollapseDetails);
+});
+
+
 // Expand/collapse tags.
 $(function() {
   function expandCollapseTags() {
