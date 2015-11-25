16b2df064ef8b040e8134a462ca5fe8e7a5e8a80
jimCresswell <jim.cresswell@gmail.com>
2015-11-25 15:38:18 +0000

    Rearranging project route for readability.

--- routes/project.js
+++ routes/project.js
@@ -21,164 +21,6 @@ var applyView = require('../lib/specifications/projects/views').applyView;
 
 var appConfig = require('../lib/configuration/app-config').get();
 
-function getGenerateLeaf(projectData) {
-  return function generateLeaf(filePath, next) {
-
-    // Fix the assumption in file-tree that we are dealing with actual
-    // files on disk.
-    filePath = path.relative(appConfig.rootPath, filePath);
-
-    // Use a loop to find the file matching this part of the tree.
-    var currentFile = projectData.files.filter(function(file) {
-      return filePath === file.filePath;
-    })[0];
-
-    // Link the file list and the tree structure by reference.
-    var leaf = {
-      name: filePath,
-      file: currentFile,
-      isFile: true
-    };
-
-    // Continue to generate the tree.
-    next(null, leaf);
-  };
-}
-
-// Use the tree to construct sets of files grouped by parent directory.
-function groupFilesByDirectory(fileTree) {
-  var filesByDir = {};
-  var treeRoot = (new TreeModel()).parse({name: 'root', children: fileTree});
-  var fileNodes = treeRoot.all(function(node) { return node.model.isFile; });
-  fileNodes.forEach(function(fileNode) {
-    var pathToNode = fileNode.getPath();
-
-    // Derive a directory path.
-    var directoryNames = pathToNode
-                          .map(function(node) {
-                            var model = node.model;
-                            if (!node.isRoot() && !model.isFile) {
-                              return node.model.name;
-                            }
-                            return '';
-                          });
-    var directoryPath = path.join.apply(path, directoryNames);
-
-    // Store the file data keyed by containing directory.
-    if (!filesByDir[directoryPath]) {
-      filesByDir[directoryPath] = [];
-    }
-    filesByDir[directoryPath].push(fileNode.model.file);
-  });
-
-  return filesByDir;
-}
-
-/**
- * Conditionally add edit link data to file objects.
- * @param {Object} projectData  The project data.
- * @return {Object}             The modified project data.
- */
-function addEditLinks(projectData) {
-  if (projectData.config.editUrlFormat) {
-    projectData.files.forEach(function(file) {
-      var editUrlTemplate = handlebars.compile(projectData.config.editUrlFormat);
-      var editUrl = editUrlTemplate({
-        repoUrl: projectData.repoUrl,
-        repoUrlWithoutGitSuffix: projectData.repoUrl.replace(/\.git$/i, ''),
-        branchName: projectData.currentShortBranchName,
-        pathToFile: file.filePath
-      });
-
-      file.editUrl = editUrl;
-    });
-  }
-  return projectData;
-}
-
-// Render the project page and send to client.
-function getRender(res, appConfig, renderOptions) {
-  return function render(projectData) {
-    var renderingData = {};
-
-    // The tags object associated with this project.
-    var projectTags = {};
-
-    renderingData.openBurgerMenu = renderOptions.openBurgerMenu;
-    renderingData.currentViewName = renderOptions.currentViewName;
-
-    // Handle no project data being found.
-    if (!projectData) {
-      res.render('project', renderingData);
-      return;
-    }
-
-    // Create a reference to the project data on
-    // the object that will be passed to the
-    // template.
-    renderingData.project = projectData;
-
-    // If there are no files in the project then don't
-    // try and get file contents.
-    if (!projectData.files.length) {
-      res.render('project', renderingData);
-      return;
-    }
-
-    // Applying views from configuration.
-    // Chrome hasn't turned destructuring assignment on yet,
-    // so I'm cheating
-    let ret = applyView(projectData, renderingData);
-    projectData = ret[0];
-    renderingData = ret[1];
-
-    // Configure function for mapping file paths to file data and use it.
-    var pathToData = processFiles.getFilePathToFileObject(appConfig.projectRoute, projectData, getFileContent);
-    projectData.files = projectData.files.map(pathToData);
-
-
-    // Wait for content promises to resolve.
-    // We don't actually care about the promise values here, they are already
-    // part of the file object, we just need them all fulfilled or rejected.
-    var promisesForFileContent = projectData.files.map(function(f) {return f.contentPromise;});
-    return Promise.all(promisesForFileContent)
-      .then(function() {
-
-        // Mix in the file content.
-        projectData.files = projectData.files.map(processFiles.processFileContent);
-
-        // Conditionally add the edit links to individual files.
-        projectData = addEditLinks(projectData);
-
-        // Filer the features and scenarios by requested tag name.
-        let ret = filterByTag(projectData, projectTags, renderOptions.currentTags);
-        projectData = ret[0];
-        projectTags = ret[1];
-
-        /*
-          Generate a tree data structure from the flat file list.
-          The rest of this function is part of the
-          callback to arrrayToTree.
-         */
-        var fileList = projectData.files.map(function(file) { return file.filePath; });
-        arrrayToTree(fileList, getGenerateLeaf(projectData), function postTreeConstruction(err, fileTree) {
-
-          // Reference the finished file tree on the rendering data object.
-          renderingData.project.filesByDir = groupFilesByDirectory(fileTree);
-
-          // Render the page.
-          res.render('project', renderingData);
-        });
-      });
-  };
-}
-
-// Pass errors to the next Express middleware for handling.
-function getPassError(next) {
-  return function passError(err) {
-    next(err);
-  };
-}
 
 // List of available features in a project.
 router.get(/^\/([^\/]+)$/, function(req, res, next) {
@@ -262,5 +104,167 @@ router.get(/^\/([^\/]+)$/, function(req, res, next) {
       .catch(configuredPassError);
   }
 });
+
+// Pass errors to the next Express middleware for handling.
+function getPassError(next) {
+  return function passError(err) {
+    next(err);
+  };
+}
+
+// Render the project page and send to client.
+function getRender(res, appConfig, renderOptions) {
+  return function render(projectData) {
+    var renderingData = {};
+
+    // The tags object associated with this project.
+    var projectTags = {};
+
+    renderingData.openBurgerMenu = renderOptions.openBurgerMenu;
+    renderingData.currentViewName = renderOptions.currentViewName;
+
+    // Handle no project data being found.
+    if (!projectData) {
+      res.render('project', renderingData);
+      return;
+    }
+
+    // Create a reference to the project data on
+    // the object that will be passed to the
+    // template.
+    renderingData.project = projectData;
+
+    // If there are no files in the project then don't
+    // try and get file contents.
+    if (!projectData.files.length) {
+      res.render('project', renderingData);
+      return;
+    }
+
+    // Applying views from configuration.
+    // Chrome hasn't turned destructuring assignment on yet,
+    // so I'm cheating
+    let ret = applyView(projectData, renderingData);
+    projectData = ret[0];
+    renderingData = ret[1];
+
+    // Configure function for mapping file paths to file data and use it.
+    var pathToData = processFiles.getFilePathToFileObject(appConfig.projectRoute, projectData, getFileContent);
+    projectData.files = projectData.files.map(pathToData);
+
+
+    // Wait for content promises to resolve.
+    // We don't actually care about the promise values here, they are already
+    // part of the file object, we just need them all fulfilled or rejected.
+    var promisesForFileContent = projectData.files.map(function(f) {return f.contentPromise;});
+    return Promise.all(promisesForFileContent)
+      .then(function() {
+
+        // Mix in the file content.
+        projectData.files = projectData.files.map(processFiles.processFileContent);
+
+        // Conditionally add the edit links to individual files.
+        projectData = addEditLinks(projectData);
+
+        // Filer the features and scenarios by requested tag name.
+        let ret = filterByTag(projectData, projectTags, renderOptions.currentTags);
+        projectData = ret[0];
+        projectTags = ret[1];
+
+        /*
+          Generate a tree data structure from the flat file list.
+          The rest of this function is part of the
+          callback to arrrayToTree.
+         */
+        var fileList = projectData.files.map(function(file) { return file.filePath; });
+        arrrayToTree(fileList, getGenerateLeaf(projectData), function postTreeConstruction(err, fileTree) {
+
+          // Reference the finished file tree on the rendering data object.
+          renderingData.project.filesByDir = groupFilesByDirectory(fileTree);
+
+          // Render the page.
+          res.render('project', renderingData);
+        });
+      });
+  };
+}
+
+
+function getGenerateLeaf(projectData) {
+  return function generateLeaf(filePath, next) {
+
+    // Fix the assumption in file-tree that we are dealing with actual
+    // files on disk.
+    filePath = path.relative(appConfig.rootPath, filePath);
+
+    // Use a loop to find the file matching this part of the tree.
+    var currentFile = projectData.files.filter(function(file) {
+      return filePath === file.filePath;
+    })[0];
+
+    // Link the file list and the tree structure by reference.
+    var leaf = {
+      name: filePath,
+      file: currentFile,
+      isFile: true
+    };
+
+    // Continue to generate the tree.
+    next(null, leaf);
+  };
+}
+
+
+// Use the tree to construct sets of files grouped by parent directory.
+function groupFilesByDirectory(fileTree) {
+  var filesByDir = {};
+  var treeRoot = (new TreeModel()).parse({name: 'root', children: fileTree});
+  var fileNodes = treeRoot.all(function(node) { return node.model.isFile; });
+  fileNodes.forEach(function(fileNode) {
+    var pathToNode = fileNode.getPath();
+
+    // Derive a directory path.
+    var directoryNames = pathToNode
+                          .map(function(node) {
+                            var model = node.model;
+                            if (!node.isRoot() && !model.isFile) {
+                              return node.model.name;
+                            }
+                            return '';
+                          });
+    var directoryPath = path.join.apply(path, directoryNames);
+
+    // Store the file data keyed by containing directory.
+    if (!filesByDir[directoryPath]) {
+      filesByDir[directoryPath] = [];
+    }
+    filesByDir[directoryPath].push(fileNode.model.file);
+  });
+
+  return filesByDir;
+}
+
+
+/**
+ * Conditionally add edit link data to file objects.
+ * @param {Object} projectData  The project data.
+ * @return {Object}             The modified project data.
+ */
+function addEditLinks(projectData) {
+  if (projectData.config.editUrlFormat) {
+    projectData.files.forEach(function(file) {
+      var editUrlTemplate = handlebars.compile(projectData.config.editUrlFormat);
+      var editUrl = editUrlTemplate({
+        repoUrl: projectData.repoUrl,
+        repoUrlWithoutGitSuffix: projectData.repoUrl.replace(/\.git$/i, ''),
+        branchName: projectData.currentShortBranchName,
+        pathToFile: file.filePath
+      });
+
+      file.editUrl = editUrl;
+    });
+  }
+  return projectData;
+}
 
 module.exports = router;
