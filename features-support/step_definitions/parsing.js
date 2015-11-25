215866d15fbe42141a20b78420a865b7f48b933d
jimCresswell <jim.cresswell@gmail.com>
2015-11-25 10:39:08 +0000

    Stuff

--- features-support/step_definitions/parsing.js
+++ features-support/step_definitions/parsing.js
@@ -71,7 +71,7 @@ module.exports = function() {
       var scenario = features.scenarioDefinitions[scenarioIndex];
       var scenarioValues;
 
-      // If the suboject key is specified dig the values out of the objects.
+      // If the suboject key is specified dig the values out of the objects
       // e.g. get the names of steps out of an array of steps.
       // c.f. https://lodash.com/docs#pluck .
       if (key2) {
