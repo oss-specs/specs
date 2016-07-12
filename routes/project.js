f1bf27cdf0d1e3bbb75e37d3979b9192e3c3263b
N440708 <ben.summerfield-crowe@ba.com>
2016-07-12 13:46:40 +0100

    Decoupling from jenkins and changing the expect json from specs.json for ci

--- routes/project.js
+++ routes/project.js
@@ -132,9 +132,19 @@ function getRender(res, appConfig, renderOptions) {
     renderingData.openBurgerMenu = renderOptions.openBurgerMenu;
     renderingData.currentProjectViewName = renderOptions.currentProjectViewName;
     renderingData.tagRequested = !!renderOptions.currentTags;
-    //THIS feels like I should move it to another method
-    if(renderOptions.shouldGetResults && projectData.config.jenkinsJobs){
-      getResults(projectData);
+    //TODO this is optional yet will currently crash page if not there, fix
+    if(projectData.config.jenkinsJobs) {
+      renderingData.jenkinsURLs = true;
+    }
+
+    if(false) {
+      renderingData.teamCityURLs = true;
+    }
+
+    if(renderOptions.shouldGetResults){
+      if(renderOptions.shouldGetResults==='jenkins') {
+        getResults(projectData, 'jenkins');
+      }
     }
     // Handle no project data being found.
     if (!projectData) {
@@ -329,66 +339,6 @@ function getPassError(next) {
   return function passError(err) {
     next(err);
   };
-}
-
-/**
- * Hits a url, calls a call back function and returns an array
- */
-function httpGet(theUrl, callBack, async) {
-  var xmlHttp = new XMLHttpRequest();
-  var resultsList = [];
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      resultsList=callBack(xmlHttp.responseText, theUrl);
-    }
-  };
-  xmlHttp.open('GET', theUrl, async);
-  xmlHttp.send(null);
-  return resultsList;
-}
-
-/**
- * Used as a callback function
- * Gets the list of jobs from jenkins and then uses that list to get a list of features
- * @param responseText    the json response from jenkins
- * @param url             the url for the jenkins area we are checking
- */
-function ParseJsonJobs(responseText,url) {
-  var jobsList = [];
-  var jsonText = JSON.parse(responseText);
-  var jobsJson = jsonText['jobs'];
-  for(var i = 0; i < jobsJson.length; i++){
-    jobsList.push(jobsJson[i]['name']);
-  }
-  url = url.replace(/api\/json\?depth=1&tree=jobs\[name]/g,'');
-  for(var j = 0; j < jobsList.length; j++){
-    httpGet(url+'job/'+jobsList[j]+'/lastCompletedBuild/testReport/api/json?pretty=true',ParseJsonIndividualJobs,true);
-  }
-}
-
-/**
- * Used as a call back function
- * Gets the list of ran features from jenkins and adds them to a global variable jobNames
- */
-function ParseJsonIndividualJobs(responseText,url) {
-  var jobsList = [];
-  var jsonText = JSON.parse(responseText);
-  var jobsJson = jsonText['suites'];
-  var i = jobsJson.length-1;
-  for(var j = 0; j < jobsJson[i]['cases'].length; j++){
-    var jobJson =jobsJson[i]['cases'][j];
-    jobJson.url = url;
-    jobsList.push(jobJson);
-  }
-  // allJobs.push.apply(allJobs,jobsList);
-  // appConfig.jobNames = allJobs;
-
-  appConfig.jobNames.push.apply(appConfig.jobNames,jobsList);
-}
-
-function httpGetJobs(url) {
-  //Using async right now as is quicker and seems to be working, change to false if want results to come in same order each time
-  httpGet(url+'api/json?depth=1&tree=jobs[name]', ParseJsonJobs,true);
 }
 
 module.exports = router;
